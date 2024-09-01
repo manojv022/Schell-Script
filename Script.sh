006bef83f0e1d4f2c25b9b4d6001fc8c2dd4ad0d
manojv022 <167419795+manojv022@users.noreply.github.com>
2024-09-02 00:30:47 +0530

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -1,3 +1,10 @@
+### BASH Shell Script:
+
+#vi <filename.sh> : to create a script file
+
+# bash <file name>  : to run script
+
+----------------------------------------
 
 ### 1. User Intrection :
 
@@ -5,4 +12,63 @@
 read -p "what is your name?" name
 echo "your name is $name"
 
+
+
 -----------------------------------------
+### 2. Math Calculation:
+
+#!/bin/bash
+
+x=10
+y=15
+
+let mul=$x*$y
+echo "$mul"
+
+let sum=$x+$y
+echo "$sum"
+----------------
+### 2nd Directly withput let command:
+
+
+#!/bin/bash
+
+x=10
+y=15
+
+echo "$(($x+$y))"
+
+echo "$(($x*$y))"
+
+echo "$(($x/$y))"
+
+echo "$(($x-$y))"
+
+----------------------------------------------------------------
+
+### 3. Conditional Support:
+
+#!/bin/bash
+
+read -p "enter your marks: " marks
+
+if [[ $marks -ge 80 ]]
+then
+        echo "1st devesion"
+elif [[ $marks -ge 60 ]]
+
+then
+        echo "2nd devesion"
+elif [[ $marks -ge 40 ]]
+then
+        echo "3rd devesion"
+
+else
+        echo "you are fail!!!!!!!!!!!!!!!!"
+fi
+
+--------------------------------------------------------------------------
+
+
+### 4. 
+
