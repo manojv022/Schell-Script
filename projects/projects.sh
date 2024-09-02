e6d58721b9fe9f5ee597e15eddfeef4e17420620
manojv022 <167419795+manojv022@users.noreply.github.com>
2024-09-03 01:35:07 +0530

    Update projects.sh

--- projects/projects.sh
+++ projects/projects.sh
@@ -52,3 +52,60 @@ do
 done
 
 -------------------------------------------------------------------
+
+
+
+### User Add and password creation:
+
+
+
+#!/bin/bash
+
+# Check if the script is run as root
+if [[ "${UID}" -ne 0 ]]; then
+    echo 'Please run with sudo or as root.'
+    exit 1
+fi
+
+# Ensure at least one argument (the username) is provided
+if [[ "${#}" -lt 1 ]]; then
+    echo "Usage: ${0} USER_NAME [COMMENT]..."
+    echo 'Create a user with name USER_NAME and an optional COMMENT field.'
+    exit 1
+fi
+
+USER_NAME="${1}"
+shift
+COMMENT="${@}"
+
+# Generate a password based on the current date and time
+PASSWORD=$(date +%s%N | sha256sum | head -c 12)
+
+# Create the user with the provided comment
+useradd -c "${COMMENT}" -m "${USER_NAME}"
+
+if [[ $? -ne 0 ]]; then
+    echo 'The account could not be created.'
+    exit 1
+fi
+
+# Set the password for the user
+echo "${USER_NAME}:${PASSWORD}" | chpasswd
+
+if [[ $? -ne 0 ]]; then
+    echo 'Password could not be set.'
+    exit 1
+fi
+
+# Force password change on next login
+passwd -e "${USER_NAME}"
+
+echo
+echo "Username: ${USER_NAME}"
+echo
+echo "Password: ${PASSWORD}"
+echo
+echo "Hostname: $(hostname)"
+
+
+--------------------------------------------------------------------------------
