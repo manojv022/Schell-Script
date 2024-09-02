93eaa683c84bdbc2496dcb9baa53ebd9f5f4b437
manojv022 <167419795+manojv022@users.noreply.github.com>
2024-09-02 23:04:54 +0530

    Update projects.sh

--- projects/projects.sh
+++ projects/projects.sh
@@ -1,1 +1,15 @@
+### Free RAM Status Checking:
 
+#!/bin/bash
+
+FREE_SPACE=$( free -mt | grep "Total" | awk '{print $4}')
+TH=9700
+
+if [[ $FREE_SPACE -lt $TH ]]
+then
+        echo "WARNING, RAM is running low"
+else
+        echo "RAM is sufficient - $FREE_SPACE M"
+fi
+
+---------------------------------------------
