fcd0f4fc4559f21f1f6424a0526918476bafdc20
manojv022 <167419795+manojv022@users.noreply.github.com>
2024-09-03 00:18:35 +0530

    Update projects.sh

--- projects/projects.sh
+++ projects/projects.sh
@@ -13,3 +13,42 @@ else
 fi
 
 ---------------------------------------------
+
+
+### Archive files Bigger theen 20kb:
+
+#!/bin/bash
+#Revision:001$
+#$tue sep 02 23:51 IST 2024$
+
+#Variables
+BASE=/home/manoj/tutorials/find_commands
+DAYS=10
+DEPTH=1
+RUN=0
+
+#Check if the directory is present or not
+if [ ! _d $BASE ]
+then
+        echo "directory does not exist: $BASE"
+        exit 1
+fi
+
+#Create 'archive' folder if not present
+if [ ! -d $BASE/archive ]
+then
+        mkdir $BASE/archive
+fi
+
+#Find the list of files larget then +20K
+for i in 'find $BASE -maxdepth $DEPTH -type f -size +20Mb'
+do
+        if [ $RUN -eq 0 ]
+        then
+                echo "[$(date "+Y_%m_%d %H:%M:%S")] archiving $i ==> $BASE/archive"
+                gzip $i || exit 1
+                mv $i.gz $BASE/archive || exit 1
+        fi
+done
+
+-------------------------------------------------------------------
