ec89120a2860a41fa7e06fc28baac72f6aa16f63
Manoj Vishnoi <167419795+manojv022@users.noreply.github.com>
2025-03-24 09:10:50 +0530

    Update postgres.sh

--- Data-Base/postgres.sh
+++ Data-Base/postgres.sh
@@ -1,1 +1,35 @@
 
+
+
+#!/bin/bash
+
+PGUSER="postgres"
+PGPASSWORD="passwd"
+PGHOST="localhost"
+PGPORT="5432"
+PGDATABASE="my_db"
+BACKUP_DIR="/var/backup/postgres_backup"
+
+mkdir -p "$/var/backup/postgres_backup"
+
+CURRENT_DATE=$(date +"%Y%m%d")
+BACKUP_FILE="$BACKUP_DIR/backup_mydb${PGDB}_${CURRENT_DATE}.sql"
+
+# Dump the database
+pg_dump -U "$PGUSER" -h "$PGHOST" -p "$PGPORT" "$PGDB" > "$BACKUO_FILE"
+
+
+if [ $? -eq 0 ]; then
+  echo "Backup successful: $BACKUP_FILE"
+else
+  echo "Backup failed!" >&2
+  exit 1
+fi
+
+# Archive backup files older than 30 days
+
+find "$BACKUP_DIR" -type f -mtime +30 -exec tar -czf "$ARCHIVE_DIR/$(basename {} .sql)_$(date -r {} +"%Y%m%d").tar.gz" {} \;
+
+ 
+if [ $? -eq 0 ]; then
+  echo "Archive successful"
